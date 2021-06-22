5c5c083726ce88ac7eaea7cab17dc053f1969d9e
Yash N. Shah <yash9414@gmail.com>
2021-06-23 00:49:47 +0530

    Update run.sh
    Fixing classpath

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 
 #java -cp bin uml.UmlGenerator java.util.{ArrayList,LinkedList,Queue,Stack,Deque} > ~/test/uml.dot && dot ~/test/uml.dot -Tsvg -o ~/test/uml.svg && open ~/test/uml.svg
 
-java -cp bin uml.UmlGenerator "$@" > ~/test/uml.dot && dot ~/test/uml.dot -Tsvg -o ~/test/uml.svg && open ~/test/uml.svg
+mkdir -p ~/test; java -cp target/classes uml.UmlGenerator "$@" > ~/test/uml.dot && dot ~/test/uml.dot -Tsvg -o ~/test/uml.svg && open ~/test/uml.svg
